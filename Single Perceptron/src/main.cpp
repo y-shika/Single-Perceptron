8677b0b22b571c2ca7c2c83a36146bbaf4927817
y-shika <shikanai327327@gmail.com>
2018-04-09 23:28:44 +0900

    refactoring

--- Single Perceptron/src/main.cpp
+++ Single Perceptron/src/main.cpp
@@ -4,8 +4,8 @@ int main(int argc, char * argv[]) {
 	SinglePerceptron SinglePer;
 	SinglePer.learn();
 
-	for (int i = 0; i < SinglePer.w_log.size(); i++)
-		std::cout << SinglePer.w_log[i][0] << ", " << SinglePer.w_log[i][1] << ", " << SinglePer.p_log[i] << std::endl;
+	/*for (int i = 0; i < SinglePer.w_log.size(); i++)
+		std::cout << SinglePer.w_log[i][0] << ", " << SinglePer.w_log[i][1] << ", " << SinglePer.p_log[i] << std::endl;*/
 
 	system("pause");
 
